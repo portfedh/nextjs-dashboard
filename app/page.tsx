4359da43db7cc1fa02114a0ced7c7b52b3e5dff0
Pablo Cruz Lemini <portfedh@gmail.com>
2025-08-12 11:10:52 -0600

    Add new SVG logo and images for S2G Energy
    - Created a new SVG file for the S2G Energy logo with detailed paths and styling.
    - Added a dark version of the S2G Energy logo as a PNG file.
    - Included a new contact image in WEBP format for improved loading performance.

--- app/page.tsx
+++ app/page.tsx
@@ -25,38 +25,32 @@ export default function Page() {
           className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
         /> */}
           {/* Example of css module */}
-          <div className={styles.shape} />
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Bienvenido a S2G Energy</strong>
           </p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Acceder</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="Screenshots of the dashboard project showing mobile version"
+            src="/contact.webp"
+            width={640}
+            height={640}
+            className="object-contain"
+            style={{
+              maxWidth: 640,
+              maxHeight: 640,
+              width: "100%",
+              height: "auto",
+            }}
+            alt="Contact hero image"
           />
         </div>
       </div>
--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,18 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import Image from "next/image";
+import { lusitana } from "@/app/ui/fonts";
 
 export default function AcmeLogo() {
   return (
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+      <Image
+        src="/S2G_Energy.79207f7f.svg"
+        alt="S2G Energy Logo"
+        width={215}
+        height={45}
+        className="mr-2"
+      />
     </div>
   );
 }
